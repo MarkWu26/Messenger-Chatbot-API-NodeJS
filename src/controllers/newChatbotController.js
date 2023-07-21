321c5a1fa42f46fc4a027e184965fd3904cd6766
lnxe-bit <lance.yzn@gmail.com>
2023-07-21 14:01:56 +0800

    Bug fix

--- src/controllers/newChatbotController.js
+++ src/controllers/newChatbotController.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 
-import request from "request";
 import aboutUs from "../responses/aboutUs.json"
 import natureOfBusiness from "../responses/natureOfBusiness.json"
 import policiesAndProjects from "../responses/policiesAndProjects.json"
@@ -13,12 +12,73 @@ import services from "../responses/services.json"
 import accomplishedProjects from "../responses/accomplishedProjects.json"
 import history from "../responses/history.json"
 
+const setGetStartedButton = async()=>{
+    try {
+        const PAGE_ACCESS_TOKEN = process.env.VERIFY_ACCESS_TOKEN;
+      const response = await fetch(`https://graph.facebook.com/v17.0/me/messenger_profile?access_token=${PAGE_ACCESS_TOKEN}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          get_started: {
+            payload: 'GET_STARTED_PAYLOAD', 
+          },
+        }),
+      });
+  
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.error('Error setting Get Started button:', error);
+    }
+  }
+
+setGetStartedButton();
+
+const refreshPing = async () =>{
+        console.log("refreshing ping")
+        try{
+           
+            console.log("trying to refresh ping")
+            const APP_ID = process.env.APP_ID;
+            const VERIFY_ACCESS_TOKEN = process.env.VERIFY_ACCESS_TOKEN
+            const response = await fetch(`https://graph.facebook.com/${APP_ID}?fields=id&access_token=${VERIFY_ACCESS_TOKEN}`);
+            const data = await response.json();
+    
+           if(data.id === APP_ID){
+            console.log('Ping Refresh successful. Chatbot is active');
+           }
+           else{
+            console.log('Ping refresh failed. Chatbot might be inactive or there is an issue');
+           }
+        }
+       catch(err){
+        console.error("Error during ping check", err);
+        if(err.code === 'ETIMEDOUT' && err.syscall === 'write'){
+            console.log("retrying ping refresh...")
+            await new Promise ((resolve)=> setTimeout(resolve, 1000));
+            return refreshPing();
+        }
+       }
+}
+
+    
+const startPing = () =>{
+    console.log("Starting ping...");
+    refreshPing();
+    console.log("setting timeout 1");
+    setInterval(refreshPing, 1 * 60 * 1000);
+    console.log("setting timeout 2");
+}
+
+startPing(); 
+
 let isPersistentMenuSet = false;
 
-const callMessengerAPI = async (request_body) => {
+ const callMessengerAPI = async (request_body) => {
     console.log("Calling Messenger API")
     try {
-      const { default: fetch } = await import('node-fetch');
       const VERIFY_ACCESS_TOKEN = process.env.VERIFY_ACCESS_TOKEN
       const response = await fetch(`https://graph.facebook.com/v17.0/me/messenger_profile?access_token=${VERIFY_ACCESS_TOKEN}`, {
         method: "POST",
@@ -63,7 +123,7 @@ const setPersistentMenu = () =>{
 }
 
 setPersistentMenu(); 
-
+ 
 const homePage = (req, res) => {
     return res.send("Welcome to messenger chatbot")
 }
@@ -94,20 +154,20 @@ const postWebhook = (req, res) => {
         const sender_psid = webhookEvent.sender.id;
         console.log(`sender Id: ${sender_psid}`);
 
+        if(webhookEvent.message){
+            handleMessage(sender_psid, webhookEvent.message);
+         }
+         else if(webhookEvent.postback){
+            handlePostBack(sender_psid, webhookEvent.postback);
+         }
+    });
+
         // Call setPersistentMenu initially
       if (isPersistentMenuSet === false) {
         setPersistentMenu();
         isPersistentMenuSet = true;
       }
 
-        if(webhookEvent.message){
-           handleMessage(sender_psid, webhookEvent.message);
-           console.log("handling message")
-        }
-        else if(webhookEvent.postback){
-           handlePostBack(sender_psid, webhookEvent.postback)
-        }
-        });
         res.status(200).send('EVENT RECEIVED');
 
     } else {
@@ -142,7 +202,6 @@ const sendWelcomeMessage = (sender_psid) =>{
             }
         }
     }
-
     callSendAPI(sender_psid, response)
 }
 
@@ -228,9 +287,6 @@ const handlePostBack = (sender_psid, received_postback) => {
             }
         break;
     }
-
-   
-
     callSendAPI(sender_psid, response)
 }
 
@@ -253,7 +309,7 @@ const handleMessage = (sender_psid, received_message) => {
         const isKeywordpolicyproj = policyProjKeywords.some(keyword => Message.includes(keyword));
 
         //thank you keywords
-        const thankyouKeyword = ["thank you", "thank", "thankyou", "thanks", "salamat", "tenx", "thanz", "thankz", "tenks"]; //ty
+        const thankyouKeyword = ["thank you", "thank", "thankyou", "thanks", "salamat", "tenx", "thanz", "thankz", "tenks", "thank you so much"]; //ty
         const isKeyWordthankyou = thankyouKeyword.some(keyword => Message.includes(keyword));
 
         //hiring keywords
@@ -277,9 +333,21 @@ const handleMessage = (sender_psid, received_message) => {
         const supervisorKeyword = ["supervisor", "executive", "executives"];
         const isKeywordSupervisor = supervisorKeyword.some(keyword => Message.includes(keyword));
 
+        //
+        const greetingKeyword = ["hi", "hello", "wassup", "good day", "good afternoon", "good morning", "kumusta", "goodafternoon", "goodmorning", "goodday", "hey", "a pleasant day", "hola", "morning", "good evening", "greetings"]
+        const isKeywordGreeting = greetingKeyword.some(keyword => Message.includes(keyword));
+
+        const affirmativeKeyword = ["okay", "noted", "ok", "alright", "roger", "copy", "roger that", "copy that", "oki", "okie", "sige"];
+        const isKeywordAffirmative = affirmativeKeyword.some(keyword => Message.includes(keyword));
+
+        const locationKeyword = ["located", "locations", "location", "locate", "locating", "situated"];
+        const iskeywordlocation = locationKeyword.some(keyword => Message.includes(keyword));
+
+        const contactKeyword = ["contact", "email", "address"];
+        const iskeywordContact = contactKeyword.some(keyword => Message.includes(keyword));
+
         const regex = /\d{11}/;
         const regexplus = /\+\d{12}/;
-
 
         if(regex.test(Message) || regexplus.test(Message)){
             response = {
@@ -288,7 +356,7 @@ const handleMessage = (sender_psid, received_message) => {
         }
         else if(isHowMuch){
             response = {
-                "text": "Good day!\n\nThank you for reaching out.\n\nMay we know if this inquiry is for commercial or educational purposes?\n\nIn addition, we would appreciate if you can give us your mobile number so that one of our personnel can contact you further.\n\nThank you."
+                "text": "Thank you for your interest in our services!\n\nTo better assist you with your inquiry about our pricing, we kindly request your mobile number.\n\nOne of our friendly personnel will reach out to you shortly to provide all the details you need. Your contact information will be treated with utmost confidentiality.\n\nThank you for choosing GSYNC Solutions!"
             }
         }
         else if(isKeywordAbout){
@@ -302,6 +370,11 @@ const handleMessage = (sender_psid, received_message) => {
         }
         else if(isKeyWordthankyou){
             response = {"text": "You're welcome! If you have any more questions or need further assistance, feel free to ask."}
+        }
+        else if(isKeywordAffirmative){
+            response = {
+                "text": "Alright! If you have any more questions or need further assistance, feel free to ask."
+            }
         }
         else if(iskeywordJob){
             response = {
@@ -323,44 +396,84 @@ const handleMessage = (sender_psid, received_message) => {
                 "text": "Here are the contact details of the Operations Supervisor:\n\nMs. Lara G. Zafranco\nOperations Supervisor, GSYNC Solutions Philippines\n0969 315 3363\nlgzafranco.gsync@gmail.com"
             }
         }
-        else{
+        else if (isKeywordGreeting){
             response = {
                 "attachment": {
                     "type": "template",
                     "payload": {
-                        "template_type": "button",
-                        "text": "Welcome to GSYNC Solutions Philippines!\n\nIf you have any other questions or need immediate support, please feel free to contact us:\n\nContact Numbers: \n+63 917 148 5103\n+63 917 544 9777\n+63 977 692 5903\n\nLandline Numbers:\n+63 54 8810167\n+63 46 2310601\n\nEmail Address:\nyourfriends@GSYNC.solutions\nGSYNC.solutions.ph@outlook.com\n\nFor further assistance, please select one of the options below: \n",
-                        "buttons" : [
-                            {
-                                "type": "postback",
-                                "title": "About Us",
-                                "payload": "about us"
-                            },
-                            {
-                                "type": "postback",
-                                "title": "Services",
-                                "payload": "services"
-                            },
-                            {
-                                "type": "postback",
-                                "title": "Policies and Projects",
-                                "payload": "policies and projects"
-                            }
-                        ]
+                        "template_type": "generic",
+                        "elements": [
+                                    {"title": "Welcome to GSYNC Solutions Philippines!\n\nIf you have any other questions or need immediate support, please feel free to contact us:\n\nContact Numbers: \n+63 917 148 5103\n+63 917 544 9777\n+63 977 692 5903\n\nLandline Numbers:\n+63 54 8810167\n+63 46 2310601\n\nEmail Address:\nyourfriends@GSYNC.solutions\nGSYNC.solutions.ph@outlook.com\n\nFor further assistance, please select one of the options below: \n",
+                                    "image_url": "https://i.ibb.co/zb05Ccv/welcome-to-gsync.png", //only good for six months free hosting only
+                                    "buttons" : [
+                                                    {
+                                                        "type": "postback",
+                                                        "title": "About Us",
+                                                        "payload": "about us"
+                                                    },
+                                                    {
+                                                        "type": "postback",
+                                                        "title": "Services",
+                                                        "payload": "services"
+                                                    },
+                                                    {
+                                                        "type": "postback",
+                                                        "title": "Policies and Projects",
+                                                        "payload": "policies and projects"
+                                                    }
+                                                 ]
+                        
+                                    }
+                                    ]
                     }
                 }
             }
+            }
+        else if (iskeywordlocation){
+            response = {
+                "text": "GSYNC Solutions Philippines is located at:\n\n8 Santol St, Liboton\n\nNaga City, Philippines 4400\n\nYou may also contact us at:\n09171485103\nyourfriends@gsync.solutions"
+            }
+        }
+        else if(iskeywordContact){
+            response = {
+                "text": "You May contact us at:\n\nContact Numbers: \n+63 917 148 5103\n+63 917 544 9777\n+63 977 692 5903\n\nLandline Numbers:\n+63 54 8810167\n+63 46 2310601\n\nEmail Address:\nyourfriends@GSYNC.solutions\nGSYNC.solutions.ph@outlook.com\n\nGSYNC Solutions Philippines is located at:\n\n8 Santol St, Liboton\n\nNaga City, Philippines 4400"
+            }
+        }
+        else{
+            response={
+                "text": "Sorry, we could not understand your inquiry."
+            }
         }
     } 
-
     callSendAPI(sender_psid, response)
 }
 
+const fetchWithExponentialBackoff = async (url, options, maxRetries =3) =>{
+    let retries = 0;
+    let delay = 1000;
 
+    while(retries < maxRetries){
+        try{
+            const response = await fetch(url, options);
+            return response;
+        }
+        catch (err){
+            if(err.type === 'request-timeout' || err.code === 'UND_ERR_CONNECT_TIMEOUT'){
+                retries++;
+                await new Promise ((resolve) => setTimeout(resolve, delay));
+                delay *= 2;
+            }
+            else {
+                throw err;
+            }
+        }
+    }
 
+    throw new Error ('Exceeded maximum retries for API Call');
+}
 
 // Sends response messages via the Send API
-const callSendAPI = (sender_psid, response) => {
+ const callSendAPI = async (sender_psid, response) => {
     console.log("trying to send message!")
 
   let request_body ={
@@ -369,25 +482,49 @@ const callSendAPI = (sender_psid, response) => {
     },
     "message": response
   }
+  const VERIFY_ACCESS_TOKEN = process.env.VERIFY_ACCESS_TOKEN;
 
-    request({
-    "uri": "https://graph.facebook.com/v17.0/me/messages",
-    "qs": { "access_token": process.env.VERIFY_ACCESS_TOKEN},
-    "method": "POST",
-    "json": request_body
-  }, (err, res, body) => {
-    if (!err) {
-      console.log('message sent!')
-    } else {
-      console.error("Unable to send message:" + err);
-    }
-  });   
+  try{
+    const response = await fetchWithExponentialBackoff(`https://graph.facebook.com/v17.0/me/messages?access_token=${VERIFY_ACCESS_TOKEN}`, { 
+        "method": "POST",
+        body: JSON.stringify(request_body),
+        headers: {'Content-Type': 'application/json'}
+    }, 3);
+    const result = await response.json();
+    console.log(result);
+  } catch (err){
+    console.error('Error sending message:', err)
+  }
+} 
+
+/* const callSendAPI = async (sender_psid, response) => {
+    console.log("trying to send message!");
+    let request_body ={
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+      }
+      const VERIFY_ACCESS_TOKEN = process.env.VERIFY_ACCESS_TOKEN;
+
+      try{
+        const response = await fetch(`https://graph.facebook.com/v17.0/me/messages?access_token=${VERIFY_ACCESS_TOKEN}`, { 
+            "method": "POST",
+            body: JSON.stringify(request_body),
+            headers: {'Content-Type': 'application/json'}
+        }, 3);
+        const result = await response.json();
+        console.log(result);
+      } catch (err){
+        console.error('Error sending message:', err)
+      }
+}  */
+    
 
 
 
-}
 
- 
+
 
 module.exports = {
     homePage:homePage,
